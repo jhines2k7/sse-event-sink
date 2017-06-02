fd56205fc7738806e8fa1b61e68a12337e25aa03
James Hines <jameshines10@gmail.com>
2017-06-02 07:19:38 -0400

    Adds a service class to manage the sse emitter

--- src/main/java/com/james/hines/sseeventsink/SseEventSinkApplication.java
+++ src/main/java/com/james/hines/sseeventsink/SseEventSinkApplication.java
@@ -1,13 +1,25 @@
 package com.james.hines.sseeventsink;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.SubscribableChannel;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SseEventSinkApplication {
@@ -18,13 +30,61 @@ public class SseEventSinkApplication {
 		SubscribableChannel sseevents();
 	}
 
+	@Component
+	class SseService {
+        private final List<ResponseBodyEmitter> emitters = new ArrayList<>();
+
+        SseEmitter emitter;
+
+	    public SseService() {
+            this.emitter = new SseEmitter();
+        }
+
+        void publish(String event) {
+            emitters.forEach(emitter -> {
+                try {
+                    emitter.send(event, MediaType.APPLICATION_JSON);
+                } catch (IOException e) {
+                    emitter.complete();
+                    emitters.remove(emitter);
+                }
+            });
+        }
+
+        SseEmitter getMessages() {
+            emitters.add(emitter);
+            emitter.onCompletion(() -> emitters.remove(emitter));
+            return emitter;
+        }
+    }
+
 	@EnableBinding(SseSink.class)
-	public class SseEventsSink {
-		@StreamListener(SseSink.INPUT_CHANNEL)
+	class SseEventsSink {
+	    @Autowired
+        SseService sseService;
+
+        @StreamListener(SseSink.INPUT_CHANNEL)
 		void consumeEvent(String event) {
 			LoggerFactory.getLogger(SseEventSinkApplication.class).info("Consuming event: '{}'", event);
+			sseService.publish(event);
 		}
 	}
+
+	@Controller
+    class SseEventsController {
+	    @Autowired
+	    SseService sseService;
+
+        @GetMapping("/events/subscribe")
+        public SseEmitter subscribe() {
+            return sseService.getMessages();
+        }
+
+        @GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
+        public Resource index() {
+            return new ClassPathResource("static/index.html");
+        }
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SseEventSinkApplication.class, args);
